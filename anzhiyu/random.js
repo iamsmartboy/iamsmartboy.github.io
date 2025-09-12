2016e6a180352c34b3ec7ae46db739c4695396b9
iamsmartboy <2795968395@qq.com>
2025-09-12 11:16:49 +0800

    Site updated: 2025-09-12 11:16:49

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/16107.html","posts/db3ef1dd.html"];function toRandomPost(){
+var posts=["posts/16107.html","posts/d87f7e0c.html","posts/db3ef1dd.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
